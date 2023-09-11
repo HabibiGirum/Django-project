252ce62cd8dd7ce58e09ec9c0611010ac8ec8881
Habtamu <habtamugirum0489@gmail.com>
2023-09-11 14:37:05 +0300

    env

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -12,7 +12,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from getenv import env
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+if os.environ.get("ENV_PATH"):
+    env_path = os.environ.get("ENV_PATH")
+    load_dotenv(dotenv_path=env_path)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hzys8i%z_im5l+zs$nd_js0s8w_@(dnfgdm+yhu3rx7c5t##z7'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
